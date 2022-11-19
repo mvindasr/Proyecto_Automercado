e43d58ff862085568e6851bc339c7842a7e97f29
Milton <mvindasr@ucenfotec.ac.cr>
2022-11-19 13:33:51 -0600

    Agregar plantillas base 1

---
+++ Proyecto_Automercado/Pruebas de integración/PI-02_Coleccionable_a_carrito.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Automercado.Pruebas_de_integración
+{
+    internal class Class1
+    {
+    }
+}
