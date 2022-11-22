1d65864803dd29e60a56cd6d234450feedcb29d2
Hijo De Algo <37315848+tomataso@users.noreply.github.com>
2022-11-21 19:10:57 -0600

    Mostrar Producto (En Proceso) V1
    Falta el login para poder acceder.
    Se intento refrescar pero no se logro.
    Se esta usando un metodo provisional que dio Jazz para el login, pero con mis credenciales.
    Se cambio la clase PF01 por PF066 por un conflicto.

--- Proyecto_Automercado/Pruebas funcionales/PF-02_Mostrar_producto.cs
+++ Proyecto_Automercado/Pruebas funcionales/PF-02_Mostrar_producto.cs
@@ -11,24 +11,62 @@ namespace Proyecto_Automercado
         public void Start()
         {
             driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("");
+            driver.Navigate().GoToUrl("https://automercado.cr");
             driver.Manage().Window.Maximize();
+
+            
         }
 
         [TestMethod]
+        public void loginProvicional()
+        {
+            driver.FindElement(By.Id("login")).Click();
+
+            //Busca el input de la email  y escribe el email
+            var emailInput = driver.FindElement(By.Id("email"));
+            emailInput.SendKeys("tomatejuan@gmail.com");
+
+            //Busca el input de la contraseña y escribe la contraseña
+            var passwordInput = driver.FindElement(By.Id("password"));
+            passwordInput.SendKeys("5Y5&QD6F9mzLHMm!");
+
+            //Encuentra el botón de inicio de sesión y le da click. 
+            driver.FindElement(
+                By.XPath("/html/body/ngb-modal-window[@role='dialog']/div[@role='document']//am-login-modal//" +
+                "form[@role='form']//am-button/button[@type='submit']")).Click();
+
+            //Cerrar modales -- no funciona
+            driver.FindElement(By.XPath("/body[@class='modal-open']/div[@class='introjs-overlay']")).Click();
+
+
+       
+
+        }
+
         public void Test1()
         {
+            loginProvicional();
 
-            string var1 = "";
-            string var2 = "";
+          var abarrotes = driver.FindElement(By.XPath("/html//am-main//am-home[@class='ng-star-inserted']/am-our-halls//section//div[@class='col-12']/div/div[1]/am-product-category//img[@alt='Abarrotes']"));
+           abarrotes.Click();
 
-            Assert.AreEqual(var1, var2);
+            var endulcorante = driver.FindElement(By.XPath("/html//am-main//am-product-search[@class='ng-star-inserted']/div[@class='list']/div[@class='container']/div/div[@class='col-12 col-lg-10']/div//div[@class='grid-square']/div[1]/am-product-list//div[@class='card card-product']//am-product-button//button[@type='button']"));
+            endulcorante.Click();
+
+
+            // string var1 = ""; 
+            // string var2 = "";
+
+            // Assert.AreEqual(var1, var2);
         }
 
         [TestCleanup]
         public void Fin()
         {
-            driver.Quit();
+            // driver.Quit();
         }
+
+
+
     }
 }
