c975a4efc7e3ee88e6d1241e3340e4bff970d054
Hijo De Algo <37315848+tomataso@users.noreply.github.com>
2022-11-21 21:47:16 -0600

    Mostrar Productos

--- Proyecto_Automercado/Pruebas funcionales/PF-02_Mostrar_producto.cs
+++ Proyecto_Automercado/Pruebas funcionales/PF-02_Mostrar_producto.cs
@@ -49,7 +49,9 @@ namespace Proyecto_Automercado
 
           var abarrotes = driver.FindElement(By.XPath("/html//am-main//am-home[@class='ng-star-inserted']/am-our-halls//section//div[@class='col-12']/div/div[1]/am-product-category//img[@alt='Abarrotes']"));
            abarrotes.Click();
+            // get findelements con Strings, hacer ciclo en la lista para ver el producto. Con listas.
 
+           // driver.FindElements(By.ClassName());
             var endulcorante = driver.FindElement(By.XPath("/html//am-main//am-product-search[@class='ng-star-inserted']/div[@class='list']/div[@class='container']/div/div[@class='col-12 col-lg-10']/div//div[@class='grid-square']/div[1]/am-product-list//div[@class='card card-product']//am-product-button//button[@type='button']"));
             endulcorante.Click();
 
